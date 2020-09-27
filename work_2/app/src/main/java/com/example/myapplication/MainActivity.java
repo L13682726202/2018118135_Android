2045491388bec6209f6d701c72860267417796ce
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-09-27 10:39:49 +0800

    更新实验二

--- work_2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ work_2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setTitle("MainActivity");
+
+        Button button1 = (Button) findViewById(R.id.button_1);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        Button button3 = (Button) findViewById(R.id.button_3);
+        Button button4 = (Button) findViewById(R.id.button_4);
+
+        button1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent("android.intent.action.MAIN");
+                startActivity(intent);
+            }
+        });
+    }
+
+    protected void onStart(){
+        super.onStart();
+        Log.d("MainActivity","onStart");
+    }
+
+    protected void onResume(){
+        super.onResume();
+        Log.d("MainActivity","onResume");
+    }
+
+    protected void onPause(){
+        super.onPause();
+        Log.d("MainActivity","onPause");
+    }
+
+    protected void onStop(){
+        super.onStop();
+        Log.d("MainActivity","onStop");
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("MainActivity","onDestroy");
+    }
+
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("MainActivity","onRestart");
     }
 }
