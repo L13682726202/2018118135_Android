45aa6d207dbdf8cca9a068169981af0c4b9411d9
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-09-27 22:13:20 +0800

    实验二主活动设置按钮

--- work_2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ work_2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setTitle("MainActivity");
+
+        Button button1 = (Button) findViewById(R.id.button_1);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        Button button3 = (Button) findViewById(R.id.button_3);
+        Button button4 = (Button) findViewById(R.id.button_4);
+
+        button1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent("android.intent.action.MAIN");
+                startActivity(intent);
+            }
+        });
     }
 }
