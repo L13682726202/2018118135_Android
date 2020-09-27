17fabc15fe4d2288c48722a4cb2fd6df4359fdb3
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-09-27 22:15:34 +0800

    为主活动设置生命周期函数

--- work_2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ work_2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -27,5 +28,35 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    protected void onStart(){
+        super.onStart();
+        Log.d("MainActivity","onStart");
+    }
+
+    protected void onResume(){
+        super.onResume();
+        Log.d("MainActivity","onResume");
+    }
+
+    protected void onPause(){
+        super.onPause();
+        Log.d("MainActivity","onPause");
+    }
+
+    protected void onStop(){
+        super.onStop();
+        Log.d("MainActivity","onStop");
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("MainActivity","onDestroy");
+    }
+
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("MainActivity","onRestart");
     }
 }
