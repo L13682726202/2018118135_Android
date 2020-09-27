b7ea5807f5e917a23e0d797903f2800a19dceaf3
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-09-27 22:40:15 +0800

    为活动二创建生命周期函数，并且设置按钮和按钮回调函数

--- work_2/app/src/main/java/com/example/myapplication/SecondActivity.java
+++ work_2/app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +14,70 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        this.setTitle("SecondActivity");
+
+        Button button1 = (Button) findViewById(R.id.button_1);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        Button button3 = (Button) findViewById(R.id.button_3);
+        Button button4 = (Button) findViewById(R.id.button_4);
+
+        button1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(SecondActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(SecondActivity.this,ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(SecondActivity.this,FourthActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    protected void onStart(){
+        super.onStart();
+        Log.d("SecondActivity","onStart");
+    }
+
+    protected void onResume(){
+        super.onResume();
+        Log.d("SecondActivity","onResume");
+    }
+
+    protected void onPause(){
+        super.onPause();
+        Log.d("SecondActivity","onPause");
+    }
+
+    protected void onStop(){
+        super.onStop();
+        Log.d("SecondActivity","onStop");
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("SecondActivity","onDestroy");
+    }
+
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("SecondActivity","onRestart");
     }
 }
