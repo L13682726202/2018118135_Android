d44be616be29b11e7ec17dfc411c175fd086d519
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-10-03 16:57:47 +0800

    在活动中创建按钮

--- work_3/app/src/main/java/com/example/uidesign/MainActivity.java
+++ work_3/app/src/main/java/com/example/uidesign/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.uidesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +15,23 @@ public class MainActivity extends AppCompatActivity {
 
         this.setTitle("UI Design");
 
+        Button button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+        Button button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+        Button button4 = (Button)findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+        Button button5 = (Button)findViewById(R.id.button5);
+        button5.setOnClickListener(this);
+        Button button6 = (Button)findViewById(R.id.button6);
+        button6.setOnClickListener(this);
+        Button button7 = (Button)findViewById(R.id.button7);
+        button7.setOnClickListener(this);
+
 
     }
+
+
 }
