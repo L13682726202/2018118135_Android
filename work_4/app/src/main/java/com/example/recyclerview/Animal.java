53051d6d813baa26b8f3da6936b990c23c4a80d4
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-10-05 11:17:57 +0800

    增加animal类的number属性，并对主活动和适配器类进行相应的修改

--- work_4/app/src/main/java/com/example/recyclerview/Animal.java
+++ work_4/app/src/main/java/com/example/recyclerview/Animal.java
@@ -1,17 +1,17 @@
 package com.example.recyclerview;
 
 public class Animal {
-    private int number;
+    private String number;
     private String name;
     private int imageId;
 
-    public Animal(int number, String name, int imageId){
+    public Animal(String number, String name, int imageId){
         this.number = number;
         this.name = name;
         this.imageId = imageId;
     }
 
-    public int getNumber(){
+    public String getNumber(){
         return number;
     }
 
--- work_4/app/src/main/java/com/example/recyclerview/MainActivity.java
+++ work_4/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -30,32 +30,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initAnimals(){
-        int n = 1;
         for(int i = 0; i < 2; i++){
-            Animal mouse = new Animal(n++,getRandomLengthName("鼠"), R.drawable.mouse);
-            animalList.add(mouse);
-            Animal cattle = new Animal(n++,getRandomLengthName("牛"), R.drawable.cattle);
-            animalList.add(cattle);
-            Animal tiger = new Animal(n++,getRandomLengthName("虎"), R.drawable.tiger);
-            animalList.add(tiger);
-            Animal rabbit = new Animal(n++,getRandomLengthName("兔"), R.drawable.rabbit);
-            animalList.add(rabbit);
-            Animal dragon = new Animal(n++,getRandomLengthName("龙"), R.drawable.dragon);
-            animalList.add(dragon);
-            Animal snake = new Animal(n++,getRandomLengthName("蛇"), R.drawable.snake);
-            animalList.add(snake);
-            Animal horse = new Animal(n++,getRandomLengthName("马"), R.drawable.horse);
-            animalList.add(horse);
-            Animal sheep = new Animal(n++,getRandomLengthName("羊"), R.drawable.sheep);
-            animalList.add(sheep);
-            Animal monkey = new Animal(n++,getRandomLengthName("猴"), R.drawable.monkey);
-            animalList.add(monkey);
-            Animal chicken = new Animal(n++,getRandomLengthName("鸡"), R.drawable.chicken);
-            animalList.add(chicken);
-            Animal dog = new Animal(n++,getRandomLengthName("狗"), R.drawable.dog);
-            animalList.add(dog);
-            Animal pig = new Animal(n++,getRandomLengthName("猪"), R.drawable.pig);
-            animalList.add(pig);
+            if(i == 0) {
+                Animal mouse = new Animal("1", getRandomLengthName("鼠"), R.drawable.mouse);
+                animalList.add(mouse);
+                Animal cattle = new Animal("2", getRandomLengthName("牛"), R.drawable.cattle);
+                animalList.add(cattle);
+                Animal tiger = new Animal("3", getRandomLengthName("虎"), R.drawable.tiger);
+                animalList.add(tiger);
+                Animal rabbit = new Animal("4", getRandomLengthName("兔"), R.drawable.rabbit);
+                animalList.add(rabbit);
+                Animal dragon = new Animal("5", getRandomLengthName("龙"), R.drawable.dragon);
+                animalList.add(dragon);
+                Animal snake = new Animal("6", getRandomLengthName("蛇"), R.drawable.snake);
+                animalList.add(snake);
+                Animal horse = new Animal("7", getRandomLengthName("马"), R.drawable.horse);
+                animalList.add(horse);
+                Animal sheep = new Animal("8", getRandomLengthName("羊"), R.drawable.sheep);
+                animalList.add(sheep);
+                Animal monkey = new Animal("9", getRandomLengthName("猴"), R.drawable.monkey);
+                animalList.add(monkey);
+                Animal chicken = new Animal("10", getRandomLengthName("鸡"), R.drawable.chicken);
+                animalList.add(chicken);
+                Animal dog = new Animal("11", getRandomLengthName("狗"), R.drawable.dog);
+                animalList.add(dog);
+                Animal pig = new Animal("12", getRandomLengthName("猪"), R.drawable.pig);
+                animalList.add(pig);
+            }
+            else{
+                Animal mouse = new Animal("13", getRandomLengthName("鼠"), R.drawable.mouse);
+                animalList.add(mouse);
+                Animal cattle = new Animal("14", getRandomLengthName("牛"), R.drawable.cattle);
+                animalList.add(cattle);
+                Animal tiger = new Animal("15", getRandomLengthName("虎"), R.drawable.tiger);
+                animalList.add(tiger);
+                Animal rabbit = new Animal("16", getRandomLengthName("兔"), R.drawable.rabbit);
+                animalList.add(rabbit);
+                Animal dragon = new Animal("17", getRandomLengthName("龙"), R.drawable.dragon);
+                animalList.add(dragon);
+                Animal snake = new Animal("18", getRandomLengthName("蛇"), R.drawable.snake);
+                animalList.add(snake);
+                Animal horse = new Animal("19", getRandomLengthName("马"), R.drawable.horse);
+                animalList.add(horse);
+                Animal sheep = new Animal("20", getRandomLengthName("羊"), R.drawable.sheep);
+                animalList.add(sheep);
+                Animal monkey = new Animal("21", getRandomLengthName("猴"), R.drawable.monkey);
+                animalList.add(monkey);
+                Animal chicken = new Animal("22", getRandomLengthName("鸡"), R.drawable.chicken);
+                animalList.add(chicken);
+                Animal dog = new Animal("23", getRandomLengthName("狗"), R.drawable.dog);
+                animalList.add(dog);
+                Animal pig = new Animal("24", getRandomLengthName("猪"), R.drawable.pig);
+                animalList.add(pig);
+            }
         }
     }
 
