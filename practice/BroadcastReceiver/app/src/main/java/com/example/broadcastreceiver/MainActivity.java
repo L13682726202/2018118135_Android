f332de3eb79aa08d11b660f514dcbb0e4de0f9f9
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-10-19 17:32:51 +0800

    在主活动中动态注册一个广播接收器

--- practice/BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.java
+++ practice/BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.broadcastreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.NetworkOnMainThreadException;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter intentFilter;
+    private NetworkChangeReceiver networkChangeReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver, intentFilter);
+    }
+
+    class NetworkChangeReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "network changes", Toast.LENGTH_SHORT).show();
+        }
     }
 }
