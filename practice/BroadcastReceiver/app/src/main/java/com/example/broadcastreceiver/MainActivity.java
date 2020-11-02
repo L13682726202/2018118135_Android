94c57875660e34524fc1a7b8a1e6be6e01eb14dc
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-11-02 11:13:14 +0800

    实现动态注册，监测网络的变化

--- practice/BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.java
+++ practice/BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.NetworkOnMainThreadException;
 import android.widget.Toast;
@@ -29,7 +31,14 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            Toast.makeText(context, "network changes", Toast.LENGTH_SHORT).show();
+            ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if(networkInfo != null && networkInfo.isAvailable()){
+                Toast.makeText(context,"network is available",Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
