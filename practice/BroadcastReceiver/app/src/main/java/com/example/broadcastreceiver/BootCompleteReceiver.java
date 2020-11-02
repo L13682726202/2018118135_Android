edcdd3b489532d6c5a71cbdccad9b803b5e3e77d
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-11-02 12:47:33 +0800

    完善静态类的内容

--- practice/BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/BootCompleteReceiver.java
+++ practice/BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/BootCompleteReceiver.java
@@ -1,4 +1,12 @@
 package com.example.broadcastreceiver;
 
-public class BootCompleteReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+    public void onReceive(Context context, Intent intent){
+        Toast.makeText(context, "Boot Complete", Toast.LENGTH_SHORT).show();
+    }
 }
