e79d885a2ca8c7beff5bf2429c19962a6ac532a8
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-11-11 21:19:29 +0800

    实验五-在BaseActivity类中实现广播接收器，并编写接收广播后的操作

--- work_5/app/src/main/java/com/example/login/BaseActivity.java
+++ work_5/app/src/main/java/com/example/login/BaseActivity.java
@@ -1,10 +1,19 @@
 package com.example.login;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.Loader;
 
 public class BaseActivity extends AppCompatActivity {
+
+    private ForceOfflineReceiver receiver;
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         ActivityCollector.addActivity(this);
@@ -13,5 +22,22 @@ public class BaseActivity extends AppCompatActivity {
     protected void onDestroy(){
         super.onDestroy();
         ActivityCollector.removeActivity(this);
+    }
+
+    class ForceOfflineReceiver extends BroadcastReceiver{
+        public void onReceive(final Context context, Intent intent){
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Warning");
+            builder.setMessage("You are forced to be offline. Please try to login again.");
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK",new DialogInterface.OnClickListener(){
+               public void onClick(DialogInterface dialog, int which){
+                   ActivityCollector.finishAll();
+                   Intent intent = new Intent(context, LoginActivity.class);
+                   context.startActivity(intent);
+               }
+            });
+            builder.show();
+        }
     }
 }
