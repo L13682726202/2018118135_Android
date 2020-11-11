d10f5e8923ac999c70f2771144132857a6120899
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-11-11 15:02:02 +0800

    实验五-创建BaseActivity类

---
+++ work_5/app/src/main/java/com/example/login/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.example.login;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+}
