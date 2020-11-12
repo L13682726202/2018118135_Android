c285ad238a01e4513dbd7dbc0c5920a13e1ae2b1
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-11-12 23:14:33 +0800

    实验六-创建下载线程

--- work_6/app/src/main/java/com/example/download/DownloadTask.java
+++ work_6/app/src/main/java/com/example/download/DownloadTask.java
@@ -1,5 +1,28 @@
 package com.example.download;
 
-public class DownloadTask {
+import android.os.AsyncTask;
 
+public class DownloadTask extends AsyncTask<String, Integer, Integer> {
+
+    public static final int TYPE_SUCESS = 0;
+    public static final int TYPE_FAILED = 1;
+    public static final int TYPE_PAUSED = 2;
+    public static final int TYPE_CANCELED = 3;
+
+    private DownloadListener listener;
+
+    private boolean isCanceled = false;
+
+    private boolean isPaused = false;
+
+    private int lastProgress;
+
+    public DownloadTask(DownloadListener listener){
+        this.listener = listener;
+    }
+
+    @Override
+    protected Integer doInBackground(String... strings) {
+        return null;
+    }
 }
