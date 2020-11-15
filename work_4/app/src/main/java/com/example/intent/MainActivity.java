f5cca5fec89d11ce2c0b55dd06e9ff21bae059ba
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-11-15 20:43:06 +0800

    更新实验四内容

--- work_4/app/src/main/java/com/example/intent/MainActivity.java
+++ work_4/app/src/main/java/com/example/intent/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setTitle("主活动");
+
+        Button button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = "Hello SecondActivity";
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("extra_data", data);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ work_4/app/src/main/java/com/example/intent/SecondActivity.java
@@ -0,0 +1,54 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        this.setTitle("活动二");
+
+        Button button1 = (Button)findViewById(R.id.button_1);
+        Button button2 = (Button)findViewById(R.id.button_2);
+        Button button3 = (Button)findViewById(R.id.button_3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = getIntent();
+                String data = intent.getStringExtra("extra_data");
+                Log.d("SecondActivity",data);
+                Intent intent1 = new Intent("android.intent.action.MAIN");
+                startActivity(intent1);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);
+            }
+        });
+    }
+}
