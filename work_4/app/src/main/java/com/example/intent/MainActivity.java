ff583d8d53351ba0bd38c0a8d192faf78a4612b3
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-11-15 21:08:42 +0800

    更新实验四内容

--- work_4/app/src/main/java/com/example/intent/MainActivity.java
+++ work_4/app/src/main/java/com/example/intent/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,14 +17,34 @@ public class MainActivity extends AppCompatActivity {
 
         this.setTitle("主活动");
 
-        Button button = (Button)findViewById(R.id.button);
+        Button button1 = (Button)findViewById(R.id.button_1);
+        Button button2 = (Button)findViewById(R.id.button_2);
+        Button button3 = (Button)findViewById(R.id.button_3);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String data = "Hello SecondActivity";
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                Intent intent = new Intent("android.intent.action.secondActivity");
                 intent.putExtra("extra_data", data);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
                 startActivity(intent);
             }
         });
--- work_4/app/src/main/java/com/example/intent/SecondActivity.java
+++ work_4/app/src/main/java/com/example/intent/SecondActivity.java
@@ -18,36 +18,16 @@ public class SecondActivity extends AppCompatActivity {
 
         this.setTitle("活动二");
 
-        Button button1 = (Button)findViewById(R.id.button_1);
-        Button button2 = (Button)findViewById(R.id.button_2);
-        Button button3 = (Button)findViewById(R.id.button_3);
+        Button button = (Button)findViewById(R.id.button);
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = getIntent();
                 String data = intent.getStringExtra("extra_data");
                 Log.d("SecondActivity",data);
-                Intent intent1 = new Intent("android.intent.action.MAIN");
+                Intent intent1 = new Intent(SecondActivity.this, MainActivity.class);
                 startActivity(intent1);
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("http://www.baidu.com"));
-                startActivity(intent);
-            }
-        });
-
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse("tel:10086"));
-                startActivity(intent);
             }
         });
     }
