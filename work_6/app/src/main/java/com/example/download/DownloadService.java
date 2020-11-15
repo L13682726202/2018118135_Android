c692bd3ee7fe3ec2a951cbfd60dd04e6f74e715f
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-11-15 16:49:49 +0800

    撤下work_1

--- work_6/app/src/main/java/com/example/download/DownloadService.java
+++ work_6/app/src/main/java/com/example/download/DownloadService.java
@@ -5,12 +5,44 @@ import android.content.Intent;
 import android.os.IBinder;
 
 public class DownloadService extends Service {
-    public DownloadService() {
+
+    private DownloadTask downloadTask;
+    private String downloadUrl;
+
+    private DownloadListener listener = new DownloadListener() {
+        @Override
+        public void onProgress(int progress) {
+
+        }
+
+        @Override
+        public void onSuccess() {
+
+        }
+
+        @Override
+        public void onFailed() {
+
+        }
+
+        @Override
+        public void onPaused() {
+
+        }
+
+        @Override
+        public void onCanceled() {
+
+        }
     }
+
+    private DownloadBinder
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
+
+
 }
