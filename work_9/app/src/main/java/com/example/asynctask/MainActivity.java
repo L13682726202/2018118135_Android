ebdef535723422eb41e252feb47e53979eec813c
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-11-24 21:59:13 +0800

    更新主活动中的业务逻辑，并且将DownloadTask迁移为主活动类的内部类

--- work_9/app/src/main/java/com/example/asynctask/MainActivity.java
+++ work_9/app/src/main/java/com/example/asynctask/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.asynctask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setTitle("AsyncTask");
+
+        
+    }
+
+    public class DownloadTask extends AsyncTask<Void, Integer, Boolean> {
+
+        private ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+
+        protected void onPreExecute(){
+            progressDialog.show();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try{
+                int i = 1;
+                while(true){
+                    Thread.sleep(500);
+                    publishProgress(i);
+                    if(i >= 100){
+                        break;
+                    }
+                    i++;
+                }
+            }
+            catch(Exception e){
+                return false;
+            }
+            return true;
+        }
+
+        protected void onProgressUpdate(Integer... values){
+            progressDialog.setMessage("Downloaded " + values[0] + "%");
+        }
+
+        protected void onPostExecute(Boolean result){
+            progressDialog.dismiss();
+            if(result){
+                Toast.makeText(MainActivity.this, "Download succeeded", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(MainActivity.this, "Download failed", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
