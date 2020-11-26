69eba0b2d9f6b65c51033b1247c9de76b0f8a221
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-11-26 18:14:33 +0800

    完善服务类中的代码

--- work_10/app/src/main/java/com/example/myservice/MyService.java
+++ work_10/app/src/main/java/com/example/myservice/MyService.java
@@ -3,6 +3,7 @@ package com.example.myservice;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
@@ -15,15 +16,23 @@ public class MyService extends Service {
 
     public void onCreate(){
         super.onCreate();
+        Log.d("MyService","onCreate executed");
     }
 
     public int onStartCommand(Intent intent, int flags, int startId){
-        
+        Log.d("MyService","onStartCommand executed");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("MyService","创建子线程，子线程的id号为：" + Thread.currentThread().getId());
+                stopSelf();
+            }
+        }).start();
         return super.onStartCommand(intent, flags, startId);
     }
 
     public void onDestroy(){
         super.onDestroy();
-
+        Log.d("MyService","onDestroy executed");
     }
 }
