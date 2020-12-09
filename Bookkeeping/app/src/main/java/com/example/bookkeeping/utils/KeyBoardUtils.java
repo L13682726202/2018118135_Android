100cdf72904d779a7cd2cce7a63a7acd38549c15
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-12-09 15:31:04 +0800

    修改软键盘的逻辑代码

--- Bookkeeping/app/src/main/java/com/example/bookkeeping/utils/KeyBoardUtils.java
+++ Bookkeeping/app/src/main/java/com/example/bookkeeping/utils/KeyBoardUtils.java
@@ -10,13 +10,15 @@ import android.widget.EditText;
 import com.example.bookkeeping.R;
 
 public class KeyBoardUtils {
-    private final Keyboard k1;    //自定义键盘
+    //自定义键盘
+    private final Keyboard keyboard;
     private KeyboardView keyboardView;
     private EditText editText;
 
     public interface OnEnsureListener{
         public void onEnsure();
     }
+
     OnEnsureListener onEnsureListener;
 
     public void setOnEnsureListener(OnEnsureListener onEnsureListener) {
@@ -27,9 +29,9 @@ public class KeyBoardUtils {
         this.keyboardView = keyboardView;
         this.editText = editText;
         this.editText.setInputType(InputType.TYPE_NULL);  //取消弹出系统键盘
-        k1 = new Keyboard(this.editText.getContext(), R.xml.key);
+        keyboard = new Keyboard(this.editText.getContext(), R.xml.key);
 
-        this.keyboardView.setKeyboard(k1);  //设置要显示键盘的样式
+        this.keyboardView.setKeyboard(keyboard);  //设置要显示键盘的样式
         this.keyboardView.setEnabled(true);
         this.keyboardView.setPreviewEnabled(false);
         this.keyboardView.setOnKeyboardActionListener(listener);  //设置键盘按钮被点击了的监听
@@ -47,21 +49,26 @@ public class KeyBoardUtils {
             Editable editable = editText.getText();
             int start = editText.getSelectionStart();
             switch (primaryCode) {
-                case Keyboard.KEYCODE_DELETE:   //点击了删除键
-                    if (editable!=null &&editable.length()>0) {
-                        if (start>0) {
-                            editable.delete(start-1,start);
+                //点击了删除键
+                case Keyboard.KEYCODE_DELETE:
+                    if (editable != null && editable.length() > 0) {
+                        if (start > 0) {
+                            editable.delete(start-1, start);
                         }
                     }
                     break;
-                case Keyboard.KEYCODE_CANCEL:   //点击了清零
+                //点击了清零
+                case Keyboard.KEYCODE_CANCEL:
                     editable.clear();
                     break;
-                case Keyboard.KEYCODE_DONE:    //点击了完成
-                    onEnsureListener.onEnsure();   //通过接口回调的方法，当点击确定时，可以调用这个方法
+                //点击了完成
+                case Keyboard.KEYCODE_DONE:
+                    //通过接口回调的方法，当点击确定时，可以调用这个方法
+                    onEnsureListener.onEnsure();
                     break;
-                default:  //其他数字直接插入
-                    editable.insert(start,Character.toString((char)primaryCode));
+                //其他数字直接插入
+                default:
+                    editable.insert(start, Character.toString((char)primaryCode));
                     break;
             }
         }
@@ -82,7 +89,7 @@ public class KeyBoardUtils {
         }
     };
 
-    //    显示键盘
+    //显示键盘
     public void showKeyboard(){
         int visibility = keyboardView.getVisibility();
         if (visibility == View.INVISIBLE ||visibility==View.GONE) {
@@ -90,7 +97,7 @@ public class KeyBoardUtils {
         }
     }
 
-    //    隐藏键盘
+    //隐藏键盘
     public void hideKeyboard(){
         int visibility = keyboardView.getVisibility();
         if (visibility== View.VISIBLE||visibility==View.INVISIBLE) {
