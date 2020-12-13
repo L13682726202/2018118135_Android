aed8fad2848f5c48205a1dd673c3d6c3552b9248
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-12-13 19:56:04 +0800

    编辑收入碎片的逻辑代码

--- Bookkeeping/app/src/main/java/com/example/bookkeeping/frag_record/IncomeFragment.java
+++ Bookkeeping/app/src/main/java/com/example/bookkeeping/frag_record/IncomeFragment.java
@@ -1,4 +1,24 @@
 package com.example.bookkeeping.frag_record;
 
-public class IncomeFragment {
+import java.util.List;
+
+public class IncomeFragment extends BaseRecordFragment {
+
+
+    @Override
+    public void loadDataToGV() {
+        super.loadDataToGV();
+        //获取数据库当中的数据源
+        List<TypeBean> inlist = DBManager.getTypeList(1);
+        typeList.addAll(inlist);
+        adapter.notifyDataSetChanged();
+        typeTv.setText("其他");
+        typeIv.setImageResource(R.mipmap.in_qt_fs);
+    }
+
+    @Override
+    public void saveAccountToDB() {
+        accountBean.setKind(1);
+        DBManager.insertItemToAccounttb(accountBean);
+    }
 }
