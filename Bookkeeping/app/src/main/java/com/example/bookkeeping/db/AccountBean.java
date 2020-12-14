add71c2fc9c1ca08122dedfc31d054842d3b3eec
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-12-14 15:06:37 +0800

    创建AccountBean描述记录一条数据的相关内容类

---
+++ Bookkeeping/app/src/main/java/com/example/bookkeeping/db/AccountBean.java
@@ -0,0 +1,117 @@
+package com.example.bookkeeping.db;
+
+//描述记录一条数据的相关内容类
+public class AccountBean {
+    int id;
+    //类型
+    String typename;
+    //被选中类型图片
+    int sImageId;
+    //备注
+    String remark;
+    //价格
+    float money;
+    //保存时间字符串
+    String time ;
+    int year;
+    int month;
+    int day;
+    //类型 : 支出---0  收入---1
+    int kind;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTypename() {
+        return typename;
+    }
+
+    public void setTypename(String typename) {
+        this.typename = typename;
+    }
+
+    public int getsImageId() {
+        return sImageId;
+    }
+
+    public void setsImageId(int sImageId) {
+        this.sImageId = sImageId;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public float getMoney() {
+        return money;
+    }
+
+    public void setMoney(float money) {
+        this.money = money;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getKind() {
+        return kind;
+    }
+
+    public void setKind(int kind) {
+        this.kind = kind;
+    }
+
+    public AccountBean() {
+    }
+
+    public AccountBean(int id, String typename, int sImageId, String remark, float money, String time, int year, int month, int day, int kind) {
+        this.id = id;
+        this.typename = typename;
+        this.sImageId = sImageId;
+        this.remark = remark;
+        this.money = money;
+        this.time = time;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.kind = kind;
+    }
+}
