f774b9bda559aac360416690430e62d7460e23e7
LAPTOP-HPACCCGQ\鸿鸿 <420773400@qq.com>
2020-12-16 15:31:03 +0800

    创建搜索活动，用于根据备注搜索账单信息

---
+++ Bookkeeping/app/src/main/java/com/example/bookkeeping/SearchActivity.java
@@ -0,0 +1,68 @@
+package com.example.bookkeeping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.bookkeeping.adapter.AccountAdapter;
+import com.example.bookkeeping.db.AccountBean;
+import com.example.bookkeeping.db.DBManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity {
+    ListView searchLv;
+    EditText searchEt;
+    TextView emptyTv;
+    //数据源
+    List<AccountBean>mDatas;
+    //适配器对象
+    AccountAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        initView();
+        mDatas = new ArrayList<>();
+        adapter = new AccountAdapter(this,mDatas);
+        searchLv.setAdapter(adapter);
+        //设置无数局时，显示的控件
+        searchLv.setEmptyView(emptyTv);
+    }
+
+    private void initView() {
+        searchEt = findViewById(R.id.search_et);
+        searchLv = findViewById(R.id.search_lv);
+        emptyTv = findViewById(R.id.search_tv_empty);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.search_iv_back:
+                finish();
+                break;
+            //执行搜索的操作
+            case R.id.search_iv_sh:
+                String msg = searchEt.getText().toString().trim();
+                //判断输入内容是否为空，如果为空，就提示不能搜索
+                if (TextUtils.isEmpty(msg)) {
+                    Toast.makeText(this,"输入内容不能为空！",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //开始搜索
+                List<AccountBean> list = DBManager.getAccountListByRemarkFromAccounttb(msg);
+                mDatas.clear();
+                mDatas.addAll(list);
+                adapter.notifyDataSetChanged();
+                break;
+        }
+    }
+}
